ecc853914e56a2d05d022256c81311f0da44c79a
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2019-04-18 15:08:41 +0300

    -- update vcs

--- .teamcity/vcsRoots/main.kt
+++ .teamcity/vcsRoots/main.kt
@@ -4,5 +4,6 @@ import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
 
 object BunchToolVcsRoot : GitVcsRoot({
     name = "Bunch Tool"
-    url = "file:///C:/Projects/bunches"
+    url = "https://github.com/JetBrains/bunches.git"
+    branch = "refs/heads/early_artifacts_in_composite_build"
 })
